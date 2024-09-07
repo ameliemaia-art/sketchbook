681c22b68def4003e18c57220fb6c4b8b212d623
Amelie Maia <amelierosser1986@gmail.com>
2024-09-07 14:27:45 -0400

    fux

--- website/stories/identity/identity.stories.tsx
+++ website/stories/identity/identity.stories.tsx
@@ -1,9 +1,7 @@
 import { useEffect, useRef } from "react";
 
-import createCanvas from "@utils/common/canvas";
 import IdentitySketch, { IdentityGUI } from "./identity";
 
-import "./identity.css";
 import "../global.css";
 
 import { Pane } from "tweakpane";
